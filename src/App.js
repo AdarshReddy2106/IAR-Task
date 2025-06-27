0dea2f4690d700ef5226c95af0f4a11d40f32093
Adarsh <adarshreddy2106@gmail.com>
2025-06-27 18:36:00 +0530

    Added Signup button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Signupform from './components/Signupform';
 
 function App() {
+  const [showSignup, setShowSignup] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
          Welcome
       </header>
-      <Signupform />
+      {!showSignup ? (
+        <button onClick={() => setShowSignup(true)}>Sign Up</button>
+      ) : (
+        <Signupform />
+      )}
     </div>
   );
 }
